48bfa4150baf4adc0a57d4e3572b9f90dc232b84
Tom Hutchinson <tombot@gmail.com>
2018-12-15 18:12:24 -0500

    HC,BMC

---
+++ primo-explore/custom/01TRI_INST-BMC/js/custom.js
@@ -0,0 +1,40 @@
+(function(){
+"use strict";
+'use strict';
+
+var app = angular.module('viewCustom', ['angularLoad']);
+
+// Begin BrowZine - Primo Integration...
+  window.browzine = {
+    api: "https://public-api.thirdiron.com/public/v1/libraries/168",
+    apiKey: "141dab10-b29a-4a39-b12e-fdf0a7f8d968",
+ 
+    journalCoverImagesEnabled: true,
+ 
+    journalBrowZineWebLinkTextEnabled: true,
+    journalBrowZineWebLinkText: "View Journal Contents",
+ 
+    acticleBrowZineWebLinkTextEnabled: true,
+    articleBrowZineWebLinkText: "View Issue Contents",
+ 
+    articlePDFDownloadLinkEnabled: true,
+    articlePDFDownloadLinkText: "Download PDF",
+ 
+    printRecordsIntegrationEnabled: true,
+  };
+ 
+  browzine.script = document.createElement("script");
+  browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
+  document.head.appendChild(browzine.script);
+ 
+  app.controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
+    window.browzine.primo.searchResult($scope);
+  });
+ 
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmSearchResultAvailabilityLineAfterController'
+  });
+// ... End BrowZine - Primo Integration
+
+})();
